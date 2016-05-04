6984dd7e80ba82b0ca270183cc5f3fb2e4d174c4
dizehacioglu <dh1272@nyu.edu>
2016-05-04 17:15:32 -0600

    did stuff

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,30 @@
 $(document).on('ready', function(){
 
-  $('#about').addClass('active');
+  // $('#about').addClass('active');
 
   $('nav li').on('click', function(e){
     // e.preventDefault();
-    // console.log('clicked nav');
+    console.log('clicked nav');
     $(this).addClass('active');
     $(this).siblings().removeClass('active');
   })
+
+  $('.portfolio-item').on('click', function(){
+    var portfolioWidth = $('#portfolio-graphic').width();
+    var clientWidth = document.documentElement.clientWidth;
+
+    var portfolioToggleWidth = portfolioWidth != 0 && portfolioWidth <= clientWidth/2 ? "0vw" : "50vw";
+    var bodyToggleWidth = portfolioToggleWidth === '50vw' ? '50vw' : '100vw';
+
+    $('body').children().not('aside').animate({
+      width: bodyToggleWidth
+    }, 1000);
+
+    $('#portfolio-graphic').animate({
+      width: portfolioToggleWidth
+    }, 1000);
+  })
+
+
 
 });
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/resume', function(req, res, next) {
+    console.log(req);
   res.render('resume', { title: 'Valeria Dikovistkaya' });
 });
 
